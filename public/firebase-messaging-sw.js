313905ac78f67c95b10cab2b07f2483a935170e8
Luiz <dgjheuh@gmail.com>
2026-08-12 15:05:41 -0300

    fix: add default fallback firebase config parameters in service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,17 +5,19 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-com
 
 // Inicialização com as configurações do projeto
 if (!firebase.apps.length) {
+  const urlParams = new URLSearchParams(location.search);
   firebase.initializeApp({
-    apiKey: new URL(location.href).searchParams.get("apiKey") || undefined,
-    authDomain: new URL(location.href).searchParams.get("authDomain") || undefined,
-    projectId: new URL(location.href).searchParams.get("projectId") || undefined,
-    storageBucket: new URL(location.href).searchParams.get("storageBucket") || undefined,
-    messagingSenderId: "386849385604",
-    appId: "1:386849385604:web:8c76bd4ca86d3d2ea926d1"
+    apiKey: urlParams.get("apiKey") || "AIzaSyATwNg81vq-nBJTWB_0cnhMDBuhfxYmWJA",
+    authDomain: urlParams.get("authDomain") || "cronolab-novo.firebaseapp.com",
+    projectId: urlParams.get("projectId") || "cronolab-novo",
+    storageBucket: urlParams.get("storageBucket") || "cronolab-novo.firebasestorage.app",
+    messagingSenderId: urlParams.get("messagingSenderId") || "386849385604",
+    appId: urlParams.get("appId") || "1:386849385604:web:8c76bd4ca86d3d2ea926d1"
   });
 }
 
 const messaging = firebase.messaging();
+
 
 messaging.onBackgroundMessage((payload) => {
   console.log("[firebase-messaging-sw.js] Mensagem recebida em segundo plano: ", payload);
